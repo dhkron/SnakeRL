acde97f1b21c64fd8b656421c578b0fec5cd88a1
Gil Ron <>
2017-12-10 23:52:23 +0200

    wip

---
+++ snake_game.py
@@ -0,0 +1,146 @@
+import os
+import time
+import random
+import numpy as np
+
+class P:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Game:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def start(self):
+        self.score = 0
+        self.snake = [
+            P(0, int(self.h/2)),
+            P(1, int(self.h/2)),
+            P(2, int(self.h/2)),
+        ]
+        self.candy = self.get_new_candy()
+
+    def get_snake_orientation(self):
+        return P(
+            self.snake[-1].x - self.snake[-2].x,
+            self.snake[-1].y - self.snake[-2].y,
+        )
+
+    def get_real_direction(self, direction):
+        orient = self.get_snake_orientation()
+        if direction == 0:
+           return P(
+                orient.y,
+                -orient.x,
+            )
+        elif direction == 1:
+           return P(
+                -orient.y,
+                orient.x,
+            )
+        elif direction == 2:
+            return orient
+
+    def get_new_head(self, direction):
+        head = self.snake[-1]
+        step = self.get_real_direction(direction)
+        new_head = P(
+            x=head.x+step.x,
+            y=head.y+step.y,
+        )
+        return new_head
+
+    def get_new_candy(self):
+        if self.w*self.h == len(self.snake):
+            return None
+
+        while True:
+            new_candy = P(
+                random.choice(range(self.w)),
+                random.choice(range(self.h)),
+            )
+            for scale in self.snake:
+                if new_candy.x == scale.x and new_candy.y == scale.y:
+                    break
+            else:
+                return new_candy
+
+    def step(self, direction):
+        new_head = self.get_new_head(direction)
+
+        if new_head.x < 0 or new_head.x >= self.w:
+            return True, 0
+        if new_head.y < 0 or new_head.y >= self.h:
+            return True, 0
+        for scale in self.snake[1:-2]:
+            if new_head.x == scale.x and new_head.y == scale.y:
+                return True, 0
+
+        self.snake.append(new_head)
+        if new_head.x == self.candy.x and new_head.y == self.candy.y:
+            self.candy = self.get_new_candy()
+            self.score += 1
+            if self.candy is None:
+                return True, 1
+            else:
+                return False, 1
+        else:
+            self.snake.pop(0)
+            return False, 0
+
+        raise Exception('Why am I here?')
+
+    def get_board(self):
+        board = np.zeros([self.h, self.w])
+        if self.candy is not None:
+            board[
+                self.candy.y,
+                self.candy.x,
+            ] = 255
+        for scale in self.snake:
+            board[
+                scale.y,
+                scale.x,
+            ] = 1
+        board[
+            self.snake[-1].y,
+            self.snake[-1].x,
+        ] = 2
+
+        return board
+
+    def draw(self):
+        board = self.get_board()
+
+        print('+', end='')
+        print('-' * (self.w * 2 + 1), end='')
+        print('+')
+        for row in board:
+            print('|', end=' ')
+            for col in row:
+                if col == 1:
+                    print('+', end=' '),
+                elif col == 2:
+                    print('o', end=' '),
+                elif col == 9:
+                    print('*', end=' '),
+                else:
+                    print(' ', end=' '),
+            print('|')
+        print('+', end='')
+        print('-' * (self.w * 2 + 1), end='')
+        print('+')
+
+        print('score = {score}'.format(
+            score=self.score,
+        ))
+
+    def draw_with_clear(self):
+        os.system('clear')
+        self.draw()
+    
+    def draw_with_sleep(self):
+        self.draw_with_clear()
+        time.sleep(.2)
---
+++ snake_player.py
@@ -0,0 +1,287 @@
+import os
+import time
+import random
+import collections
+import numpy as np
+import tensorflow as tf
+
+from . import snake_game
+
+EXP_BUFFER_SIZE = 100000
+EXP_BUFFER_BATCH_SIZE = 64
+MAX_EPISODES = 50000
+MAX_EPISODE_STEPS = 1000
+DISCOUNT = 0.99
+LEARNING_RATE = 1e-4
+
+class ExpBuffer:
+    def __init__(self):
+        self.buffer_size = EXP_BUFFER_SIZE
+        self.count = 0
+        self.buffer = collections.deque()
+
+    def add(self, s, a, r, s2):
+        experience = (s, a, r, s2)
+        if self.count < self.buffer_size: 
+            self.buffer.append(experience)
+            self.count += 1
+        else:
+            self.buffer.popleft()
+            self.buffer.append(experience)
+
+    def size(self):
+        return self.count
+
+    def sample_batch(self, batch_size):
+        '''     
+        batch_size specifies the number of experiences to add 
+        to the batch. If the replay buffer has less than batch_size
+        elements, simply return all of the elements within the buffer.
+        Generally, you'll want to wait until the buffer has at least 
+        batch_size elements before beginning to sample from it.
+        '''
+        batch = []
+
+        if self.count < batch_size:
+            batch = random.sample(self.buffer, self.count)
+        else:
+            batch = random.sample(self.buffer, batch_size)
+
+        s_batch = np.array([_[0] for _ in batch])
+        a_batch = np.array([_[1] for _ in batch])
+        r_batch = np.array([_[2] for _ in batch])
+        s2_batch = np.array([_[3] for _ in batch])
+
+        return s_batch, a_batch, r_batch, s2_batch
+
+    def clear(self):
+        self.deque.clear()
+        self.count = 0
+
+class Player:
+    def __init__(
+        self,
+        w,
+        h,
+    ):
+        self.w = w
+        self.h = h
+        self.exp_buffer = ExpBuffer()
+
+        tf.reset_default_graph()
+
+        self.x_board = tf.placeholder(tf.float32, shape=[None, h, w])
+
+        x_board_reshaped = tf.reshape(self.x_board, [-1, h, w, 1])
+        h_conv1 = tf.contrib.layers.conv2d(
+            inputs=x_board_reshaped,
+            num_outputs=32,
+            kernel_size=[5, 5],
+            stride=[1, 1],
+            padding='SAME',
+        )
+        h_conv1_flat = tf.reshape(h_conv1, [-1, w * h * 32])
+
+        h_fc1 = tf.contrib.layers.fully_connected(
+            inputs=h_conv1_flat,
+            num_outputs=w * h * 32,
+        )
+
+        self.Q = tf.contrib.layers.fully_connected(
+            inputs=h_fc1,
+            num_outputs=3,
+            activation_fn=None,
+        )
+        
+        self.action = tf.argmax(self.Q, 1)
+
+        self.reward_plus_discounted_next_max_Q = tf.placeholder(tf.float32, shape=[None, 3])
+        loss = tf.reduce_sum(
+            tf.square(
+                self.reward_plus_discounted_next_max_Q - self.Q,
+            ),
+        )
+        self.train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
+
+    def get_action(
+        self,
+        board_state,
+        e=0.1,
+    ):
+        action = self.sess.run(
+            self.action,
+            feed_dict={
+                self.x_board: [board_state],
+            },
+        )
+        action = action[0]
+        if np.random.rand(1) < e:
+            action = random.choice([0, 1, 2])
+
+        return action
+
+    def train_batch(
+        self,
+        states,
+        actions,
+        rewards,
+        next_states,
+    ):
+        q = self.sess.run(
+            self.Q,
+            feed_dict={
+                self.x_board: states,
+            },
+        )
+        next_q = self.sess.run(
+            self.Q,
+            feed_dict={
+                self.x_board: next_states,
+            },
+        )
+       
+        # q is is [1,3], so in order to keep loss modify just q[action]
+        for i, r in enumerate(rewards):
+            max_next_q = np.max(next_q[i,:])
+            q[i, actions[i]] = max_next_q * DISCOUNT + r
+        
+        self.sess.run(
+            [self.train_step],
+            feed_dict={
+                self.x_board: states,
+                self.reward_plus_discounted_next_max_Q: q, 
+            },
+        )
+    
+    def train_episode(
+        self,
+        game,
+        episode_number,
+    ):
+        game.start()
+        total_reward = 0
+        j = 0
+
+        state_board_buffer = collections.deque(STATE_SIZE)
+
+        for j in range(MAX_EPISODE_STEPS):
+            current_state = game.get_board()
+            current_action = self.get_action(
+                board_state=current_state,
+                e=1/(episode_number/100+10),
+            )
+
+            terminal, reward = game.step(current_action)
+            next_state = game.get_board()
+            total_reward += reward
+
+            self.exp_buffer.add(
+                current_state,
+                current_action,
+                reward,
+                next_state,
+            )
+
+            if self.exp_buffer.size() > EXP_BUFFER_BATCH_SIZE:
+                s_batch, a_batch, r_batch, s2_batch = self.exp_buffer.sample_batch(EXP_BUFFER_BATCH_SIZE)
+                self.train_batch(
+                    states=s_batch,
+                    actions=a_batch,
+                    rewards=r_batch,
+                    next_states=s2_batch,
+                )
+
+            if terminal:
+                break
+
+        return total_reward
+
+    def train(
+        self,
+    ):
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init)
+        game = snake_game.Game(self.w, self.h)
+        total_episodes_reward = 0
+        
+        for i in range(MAX_EPISODES):
+            total_episodes_reward += self.train_episode(
+                game=game,
+                episode_number=i,
+            )
+          
+            if i%100 == 0:
+                print(
+                    'Total reward for episode {episode_number}: {reward}'.format(
+                        episode_number=i,
+                        reward=total_episodes_reward/100 ,
+                    )
+                )
+                total_episodes_reward=0
+
+    def play(
+        self,
+    ):
+        while True:
+            g = snake_game.Game(self.w, self.h)
+            g.start()
+            input('Waiting for you...')
+            while True:
+                try:
+                    g.draw_with_sleep()
+                    
+                    action = self.get_action(
+                        board_state=g.get_board(),
+                        e=0,
+                    )
+                    
+                    terminal, reward = g.step(action)
+                    if terminal:
+                        if reward:
+                            g.draw_with_clear()
+                        break
+                except KeyboardInterrupt:
+                    break
+
+    @classmethod
+    def weight_variable(cls, shape):
+        initial = tf.truncated_normal(
+            stddev=0.1,
+            shape=shape,
+        )
+        return tf.Variable(initial)
+
+    @classmethod
+    def bias_variable(cls, shape):
+        initial = tf.constant(
+            value=0.1,
+            shape=shape,
+        )
+        return tf.Variable(initial)
+
+    @classmethod
+    def conv2d(cls, x, W):
+        return tf.nn.conv2d(
+            input=x,
+            filter=W,
+            strides=[1, 1, 1, 1],
+            padding='SAME',
+        )
+
+    @classmethod
+    def max_pool_2x2(cls, x):
+        return tf.nn.max_pool(
+            x,
+            ksize=[1, 2, 2, 1],
+            strides=[1, 2, 2, 1],
+            padding='SAME',
+        )
+
+
+p = Player(
+    w=4,
+    h=3,
+)
+p.train()
+p.play()
